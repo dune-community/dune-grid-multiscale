9ddccc97a5f192fe79522b1726f4996e921c2971
Felix Albrecht <felix.albrecht@uni-muenster.de>
2012-07-23 19:12:36 +0200

    [grid.multiscale.gridpart.local] added first draft of a local grid part

---
+++ dune/grid/multiscale/gridpart/local.hh
@@ -0,0 +1,137 @@
+
+#ifndef DUNE_GRID_MULTISCALE_GRIDPART_LOCAL_HH
+#define DUNE_GRID_MULTISCALE_GRIDPART_LOCAL_HH
+
+// dune-grid-multiscale
+#include <dune/grid/multiscale/gridpart/leaf.hh>
+
+namespace Dune {
+
+namespace grid {
+
+namespace Multiscale {
+
+namespace GridPart {
+
+template< class GridImp >
+class Local;
+
+template< class GridImp >
+struct LocalTraits
+  : public Dune::grid::Multiscale::GridPart::LeafTraits< GridImp >
+{
+  typedef Dune::grid::Multiscale::GridPart::LeafTraits< GridImp > BaseType;
+
+  typedef typename BaseType::GridType GridType;
+
+  typedef typename BaseType::GridPartType GridPartType;
+
+  typedef typename BaseType::IndexSetType IndexSetType;
+
+  static const PartitionIteratorType indexSetPartitionType = BaseType::indexSetPartitionType;
+
+  typedef typename BaseType::IntersectionIteratorType IntersectionIteratorType;
+
+  using BaseType::Codim;
+
+  static const bool conforming = BaseType::conforming;
+}; // class LocalTraits
+
+template< class GridImp >
+class Local
+  : public Dune::grid::Multiscale::GridPart::Default< LocalTraits< GridImp > >
+{
+public:
+  typedef Local< GridImp > ThisType;
+
+  typedef Default< LeafTraits< GridImp > > BaseType;
+
+  typedef LocalTraits< GridImp > Traits;
+
+  typedef typename Traits::GridType GridType;
+
+  typedef typename Traits::IndexSetType IndexSetType;
+
+  typedef typename Traits::IntersectionIteratorType IntersectionIteratorType ;
+
+  typedef typename IntersectionIteratorType::Intersection IntersectionType;
+
+  typedef typename GridType::template Partition< All_Partition >::LeafGridView LeafGridView;
+
+  typedef typename GridType::template Codim<0>::Entity EntityType;
+
+  typedef Dune::grid::Multiscale::GridPart::Leaf< GridType > GlobalGridPartType;
+
+  explicit Local(const GlobalGridPartType& globalGridPart)
+    : BaseType(globalGridPart.grid()),
+      globalGridPart_(globalGridPart)
+  {}
+
+  const IndexSetType& indexSet() const
+  {
+    return globalGridPart_.indexSet();
+  }
+
+  template< int codim >
+  typename BaseType::template Codim< codim >::IteratorType begin() const
+  {
+    return globalGridPart_.template begin< codim >();
+  }
+
+  template< int codim, PartitionIteratorType pitype >
+  typename Traits::template Codim< codim >::template Partition< pitype >::IteratorType begin() const
+  {
+    return globalGridPart_.template begin< codim, pitype >();
+  }
+
+  template< int codim >
+  typename BaseType::template Codim< codim >::IteratorType end() const
+  {
+    return globalGridPart_.template end< codim >();
+  }
+
+  template< int codim, PartitionIteratorType pitype >
+  typename Traits::template Codim< codim >::template Partition< pitype >::IteratorType end() const
+  {
+    return globalGridPart_.template end< codim, pitype >();
+  }
+
+  IntersectionIteratorType ibegin(const EntityType& en) const
+  {
+    return en.ileafbegin();
+  }
+
+  IntersectionIteratorType iend(const EntityType& en) const
+  {
+    return en.ileafend();
+  }
+
+  int boundaryId(const IntersectionType& intersection) const
+  {
+    return intersection.boundaryId();
+  }
+
+  int level() const
+  {
+    return globalGridPart_.level();
+  }
+
+  template< class DataHandleImp ,class DataType >
+  void communicate(CommDataHandleIF< DataHandleImp, DataType > & data, InterfaceType iftype, CommunicationDirection dir) const
+  {
+    globalGridPart_.communicate(data,iftype,dir);
+  }
+
+private:
+  const GlobalGridPartType& globalGridPart_;
+}; // class Local
+
+} // namespace GridPart
+
+} // namespace Multiscale
+
+} // namespace grid
+
+} // namespace Dune
+
+#endif // DUNE_GRID_MULTISCALE_GRIDPART_LOCAL_HH
