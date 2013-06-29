b890640ccf3adedac49e3ed85a7c23b397fd4503
Felix Albrecht <mail@felixalbrecht.de>
2013-06-29 23:17:35 +0200

    removed strange special characters

--- dune/grid/multiscale/factory/default.hh
+++ dune/grid/multiscale/factory/default.hh
@@ -1,9 +1,9 @@
-// This file is part of the dune-grid-multiscale project:
+﻿// This file is part of the dune-grid-multiscale project:
 //   http://users.dune-project.org/projects/dune-grid-multiscale
 // Copyright holders: Felix Albrecht
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-﻿#ifndef DUNE_GRID_MULTISCALE_FACTORY_DEFAULT_HH
+#ifndef DUNE_GRID_MULTISCALE_FACTORY_DEFAULT_HH
 #define DUNE_GRID_MULTISCALE_FACTORY_DEFAULT_HH
 
 #ifdef HAVE_CMAKE_CONFIG
