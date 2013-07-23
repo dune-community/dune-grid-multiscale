65e942322dae39269bfaf664a78b8946fc103e8d
Kirsten <kirsten@kirsten>
2013-07-23 16:15:38 +0200

    [dune.grid.multiscale.test] test for creating a multiscale grid with subdomains from a textfile and functions on it (FunctionFromFile) (for tumor growth model)

--- examples/provider.cc
+++ examples/provider.cc
@@ -27,6 +27,7 @@
 #include <dune/stuff/common/string.hh>
 
 // dune-grid-multiscale
+#include <dune/stuff/grid/provider.hh>
 #include <dune/grid/multiscale/provider.hh>
 
 const std::string id = "provider";
@@ -165,49 +166,153 @@ int main(int argc, char** argv)
     // grid
     info << "setting up grid:" << std::endl;
     debug.suspend();
-    typedef Dune::grid::Multiscale::ProviderInterface<> GridProviderType;
-    const GridProviderType* gridProvider
-        = Dune::grid::Multiscale::Providers<>::create(paramTree.get(id + ".grid", "grid.multiscale.provider.cube"),
-                                                      paramTree);
-    typedef GridProviderType::MsGridType MsGridType;
-    const Dune::shared_ptr< const MsGridType > msGrid = gridProvider->msGrid();
-    debug.resume();
+
+    typedef Dune::grid::Multiscale::ProviderFunctionbased<> MultiscaleGridProviderType;
+    typedef typename MultiscaleGridProviderType::NonMultiscaleType NonMultiscaleType;
+    typedef typename MultiscaleGridProviderType::GridType GridType;
+    typedef double      DomainFieldType;
+    const int           dimDomain = GridType::dimension;
+    typedef int         RangeFieldType;
+    typedef double      DiffusionRangeFieldType;
+    typedef Dune::FieldMatrix< DiffusionRangeFieldType, 3, 3> RangeType;
+
+    typedef typename Dune::Stuff::FunctionFromFile< DomainFieldType, dimDomain, RangeFieldType ,1 ,1 >
+        FunctionFromFileScalarType;
+    typedef typename Dune::Stuff::FunctionFromFile< DomainFieldType, dimDomain, DiffusionRangeFieldType ,dimDomain ,dimDomain >
+        FunctionFromFileMatrixType;
+
+    typedef Dune::Stuff::GridProviderCube< GridType > CubeProviderType;
+
+    std::string fileForFunction = "brainmask.txt";
+    std::string fileForDiffusiontensor = "tensors.txt";
+
+    typedef typename Dune::FieldVector<DomainFieldType, dimDomain> DomainType;
+    DomainType lowerLeft(0.0);
+    DomainType upperRight(117);
+    upperRight[0]=117;
+    upperRight[1]=142;
+    upperRight[2]=124;
+    std::vector< size_t > numElements = {117, 142, 124};
+
+    // function representing the diffusion tensors
+    std::shared_ptr< FunctionFromFileMatrixType > diffusion = std::make_shared< FunctionFromFileMatrixType >(fileForDiffusiontensor,
+                                                                                                            lowerLeft,
+                                                                                                            upperRight,
+                                                                                                            numElements);
+
+    // anlegen des Gitters, auf dem gerechnet werden soll mithilfe der Daten aus brain_mask
+    std::shared_ptr< CubeProviderType > gridProvider = std::make_shared< CubeProviderType >(lowerLeft,
+                                                                                            upperRight,
+                                                                                            numElements);
+
+    std::shared_ptr< GridType > grid(gridProvider->grid());
+    gridProvider->visualize();
+
+    // function representing the brainmask
+    std::shared_ptr< FunctionFromFileScalarType > function = std::make_shared< FunctionFromFileScalarType >(fileForFunction,
+                                                                                                            lowerLeft,
+                                                                                                            upperRight,
+                                                                                                            numElements);
+
+    std::vector<double> partitions = {0.01};
+    MultiscaleGridProviderType multiscaleProvider(grid, function, partitions);
+    typedef MultiscaleGridProviderType::MsGridType MsGridType;
+    const Dune::shared_ptr< const MsGridType > msGrid = multiscaleProvider.msGrid();
+    //debug.resume();
     info << "  took " << timer.elapsed()
          << " sec (has " << msGrid->globalGridPart()->grid().size(0) << " elements, "
          << msGrid->size() << " subdomains)" << std::endl;
 
     info << "visualizing... " << std::flush;
-    debug.suspend();
+    //debug.suspend();
     timer.reset();
-    gridProvider->visualize();
+    multiscaleProvider.visualize();
     info << " done (took " << timer.elapsed() << " sek)" << std::endl;
-    debug.resume();
+    //debug.resume();
 
-    info << "inspecting global grid part:" << std::endl;
-    const auto& globalGridPart = *(msGrid->globalGridPart());
-    Inspect< MsGridType::GlobalGridPartType, MsGridType::GlobalGridPartType, Dune::Stuff::Common::LogStream >
-        ::Codim< GridProviderType::dim, 0 >
-        ::entities(globalGridPart, globalGridPart, "  ", debug);
+    // visualization of the diffusion tensor
+    // make a discrete function (make 9 functions) out of it and visualize it on the grid (which one?) with the vtkwriter
+    // (auf dem grid vom gridProvider oder vom multiscaleGridProvider ? multi macht wohl mehr sinn)
+    // evtl die diskreten Funktionen so anlegen, wie den multiscaleGridProvider?! d.h. über alle Entities laufen, im
+    // center auswerten und die Werte der Matrix, die man erhält, in 9 Vektoren schreiben. d.h. als return value einen
+    // Vektor von 9 Vektoren übergeben (leere Vektoren). (das ist aber wahrscheinlich sehr sehr langsam, wenn da mit so
+    // vielen Schleifen und so vielen Zugriffen auf den Vektor gearbeitet wird.
 
-    info << "inspecting local grid parts:" << std::endl;
-    for (unsigned int subdomain = 0; subdomain < msGrid->size(); ++subdomain) {
-      info << "subdomain " << subdomain << std::endl;
-      const auto& localGridPart = *(msGrid->localGridPart(subdomain));
-      Inspect< MsGridType::GlobalGridPartType, MsGridType::LocalGridPartType, Dune::Stuff::Common::LogStream >
-          ::Codim< GridProviderType::dim, 0 >
-          ::entities(globalGridPart, localGridPart, "  ", debug);
-    }
+    // write function diffusion into 9 vectors
+    std::vector< double > diffusionVector1;
+    std::vector< double > diffusionVector2;
+    std::vector< double > diffusionVector3;
+    std::vector< double > diffusionVector4;
+    std::vector< double > diffusionVector5;
+    std::vector< double > diffusionVector6;
+    std::vector< double > diffusionVector7;
+    std::vector< double > diffusionVector8;
+    std::vector< double > diffusionVector9;
 
-    info << "inspecting boundary grid parts:" << std::endl;
-    for (unsigned int subdomain = 0; subdomain < msGrid->size(); ++subdomain) {
-      if (msGrid->boundary(subdomain)) {
-        info << "subdomain " << subdomain << std::endl;
-        const auto& boundaryGridPart = *(msGrid->boundaryGridPart(subdomain));
-        Inspect< MsGridType::GlobalGridPartType, MsGridType::BoundaryGridPartType, Dune::Stuff::Common::LogStream >
-            ::Codim< GridProviderType::dim, 0 >
-            ::entities(globalGridPart, boundaryGridPart, "  ", debug);
-      }
-    }
+    typedef typename MsGridType::GlobalGridPartType GridPartType;
+    const Dune::shared_ptr< const GridPartType> gridPart = msGrid->globalGridPart();
+    RangeType ret;
+
+    // walk the grid
+    for (typename GridPartType::Codim< 0 >::IteratorType it = gridPart->begin< 0 >(); it != gridPart->end< 0 >(); ++it) {
+      // get center of entity
+      const auto& entity = *it;
+      const DomainType center = entity.geometry().center();
+
+      diffusion->evaluate(center, ret);
+      // write the values of ret into the nine vectors --> this isn't very intelligent at the moment
+      diffusionVector1.push_back(ret[0][0]);
+      diffusionVector2.push_back(ret[0][1]);
+      diffusionVector3.push_back(ret[0][2]);
+      diffusionVector4.push_back(ret[1][0]);
+      diffusionVector5.push_back(ret[1][1]);
+      diffusionVector6.push_back(ret[1][2]);
+      diffusionVector7.push_back(ret[2][0]);
+      diffusionVector8.push_back(ret[2][1]);
+      diffusionVector9.push_back(ret[2][2]);
+    } // walk the grid
+
+    typedef typename GridPartType::GridViewType GridViewType;
+    std::cout << "visualizing diffusion tensors... " << std::flush;
+    typedef Dune::VTKWriter< GridViewType > VTKWriterType;
+    VTKWriterType vtkwriter((gridPart->gridView()));
+    // write
+    vtkwriter.addCellData(diffusionVector1, "diffusion_tensor[0][0]");
+    vtkwriter.addCellData(diffusionVector2, "diffusion_tensor[0][1]");
+    vtkwriter.addCellData(diffusionVector3, "diffusion_tensor[0][2]");
+    vtkwriter.addCellData(diffusionVector4, "diffusion_tensor[1][0]");
+    vtkwriter.addCellData(diffusionVector5, "diffusion_tensor[1][1]");
+    vtkwriter.addCellData(diffusionVector6, "diffusion_tensor[1][2]");
+    vtkwriter.addCellData(diffusionVector7, "diffusion_tensor[2][0]");
+    vtkwriter.addCellData(diffusionVector8, "diffusion_tensor[2][1]");
+    vtkwriter.addCellData(diffusionVector9, "diffusion_tensor[2][2]");
+    vtkwriter.write("diffusiontensors", Dune::VTK::ascii);
+
+//    info << "inspecting global grid part:" << std::endl;
+//    const auto& globalGridPart = *(msGrid->globalGridPart());
+//    Inspect< MsGridType::GlobalGridPartType, MsGridType::GlobalGridPartType, Dune::Stuff::Common::LogStream >
+//        ::Codim< MultiscaleGridProviderType::dim, 0 >
+//        ::entities(globalGridPart, globalGridPart, "  ", debug);
+
+//    info << "inspecting local grid parts:" << std::endl;
+//    for (unsigned int subdomain = 0; subdomain < msGrid->size(); ++subdomain) {
+//      info << "subdomain " << subdomain << std::endl;
+//      const auto& localGridPart = *(msGrid->localGridPart(subdomain));
+//      Inspect< MsGridType::GlobalGridPartType, MsGridType::LocalGridPartType, Dune::Stuff::Common::LogStream >
+//          ::Codim< MultiscaleGridProviderType::dim, 0 >
+//          ::entities(globalGridPart, localGridPart, "  ", debug);
+//    }
+
+//    info << "inspecting boundary grid parts:" << std::endl;
+//    for (unsigned int subdomain = 0; subdomain < msGrid->size(); ++subdomain) {
+//      if (msGrid->boundary(subdomain)) {
+//        info << "subdomain " << subdomain << std::endl;
+//        const auto& boundaryGridPart = *(msGrid->boundaryGridPart(subdomain));
+//        Inspect< MsGridType::GlobalGridPartType, MsGridType::BoundaryGridPartType, Dune::Stuff::Common::LogStream >
+//            ::Codim< MultiscaleGridProviderType::dim, 0 >
+//            ::entities(globalGridPart, boundaryGridPart, "  ", debug);
+//      }
+//    }
 
 //    // time grid parts
 //    info << "timing grid parts:" << std::endl;
