b94b414fa82605a7f37f7e4fb0f46f2275a6ef90
Felix Schindler <felix.schindler@wwu.de>
2014-08-27 12:31:38 +0200

    [exists] put some into lib

--- dune/grid/multiscale/exists.cc
+++ dune/grid/multiscale/exists.cc
@@ -3,19 +3,19 @@
 // Copyright holders: Felix Albrecht
 // License: BSD 2-Clause License (http://opensource.org/licenses/BSD-2-Clause)
 
-#ifndef DUNE_GRID_MULTISCALE_EXISTS_HH
-#define DUNE_GRID_MULTISCALE_EXISTS_HH
+#include "config.h"
+
+#include "exists.hh"
 
 namespace Dune {
 namespace grid {
 namespace Multiscale {
 
 
-void this_library_exists();
+void this_library_exists() {}
 
 
 } // namespace Multiscale
 } // namespace grid
 } // namespace Dune
 
-#endif // DUNE_GRID_MULTISCALE_EXISTS_HH
