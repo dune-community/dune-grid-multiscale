313e226b15a643e9c89d3cd235f54f8c486005aa
Felix Schindler <felix.schindler@wwu.de>
2015-12-10 01:10:35 +0100

    [factory.default] add first guess for NeighborRecursionLevel

--- dune/grid/multiscale/factory/default.hh
+++ dune/grid/multiscale/factory/default.hh
@@ -85,6 +85,13 @@ public:
     return 1;
   }
 };
+
+template <class Comm, ALUGridRefinementType tp>
+class NeighborRecursionLevel<Dune::ALUGrid<3, 3, Dune::simplex, tp, Comm>>
+{
+public:
+  static size_t compute() { return 9; } // just a guess!
+};
 #endif
 
 
