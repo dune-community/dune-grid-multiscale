3d67a1340afea931b5cf35393a22be8fe38c4941
Felix Schindler <felix.schindler@wwu.de>
2017-03-07 17:23:53 +0100

    [test.provider_cube] add tests for UGGrid<2>

--- dune/grid/multiscale/test/provider_cube_2d.cc
+++ dune/grid/multiscale/test/provider_cube_2d.cc
@@ -40,7 +40,7 @@ struct ExpectedResults<YaspGrid<2, EquidistantOffsetCoordinates<double, 2>>, any
             {{4, 3}, {6, 3}, {8, 3}},
             {{5, 3}, {7, 3}}};
   }
-};
+}; // ExpectedResults<YaspGrid<2, EquidistantOffsetCoordinates<double, 2>>, ...>
 
 #if HAVE_DUNE_ALUGRID
 
@@ -74,7 +74,7 @@ struct ExpectedResults<Dune::ALUGrid<2, 2, simplex, conforming>, anything>
             {{4, 3}, {6, 3}, {8, 3}},
             {{5, 3}, {7, 3}}};
   }
-};
+}; // ExpectedResults<Dune::ALUGrid<2, 2, simplex, conforming>, ...>
 
 template <bool anything>
 struct ExpectedResults<Dune::ALUGrid<2, 2, simplex, nonconforming>, anything>
@@ -106,9 +106,44 @@ struct ExpectedResults<Dune::ALUGrid<2, 2, simplex, nonconforming>, anything>
             {{4, 3}, {6, 3}, {8, 3}},
             {{5, 3}, {7, 3}}};
   }
-};
+}; // ExpectedResults<Dune::ALUGrid<2, 2, simplex, nonconforming>, ...>
 
 #endif // HAVE_DUNE_ALUGRID
+#if HAVE_DUNE_UGGRID
+
+template <bool anything>
+struct ExpectedResults<UGGrid<2>, anything>
+{
+  static std::string grid_name()
+  {
+    return "ug_2d_simplex";
+  }
+
+  static std::vector<size_t> local_sizes()
+  {
+    return {18, 18, 18, 18, 18, 18, 18, 18, 18};
+  }
+
+  static std::map<size_t, size_t> boundary_sizes()
+  {
+    return {{0, 6}, {1, 3}, {2, 5}, {3, 3}, {5, 3}, {6, 5}, {7, 3}, {8, 6}};
+  }
+
+  static std::vector<std::map<size_t, size_t>> coupling_sizes()
+  {
+    return {{{1, 3}, {3, 3}},
+            {{0, 3}, {2, 3}, {4, 3}},
+            {{1, 3}, {5, 3}},
+            {{0, 3}, {4, 3}, {6, 3}},
+            {{1, 3}, {3, 3}, {5, 3}, {7, 3}},
+            {{2, 3}, {4, 3}, {8, 3}},
+            {{3, 3}, {7, 3}},
+            {{4, 3}, {6, 3}, {8, 3}},
+            {{5, 3}, {7, 3}}};
+  }
+}; // ExpectedResults<UGGrid<2>, ...>
+
+#endif // HAVE_DUNE_UGGRID
 
 
 // clang-format off
@@ -116,6 +151,9 @@ typedef ::testing::Types< YaspGrid<2, EquidistantOffsetCoordinates<double, 2>>
 #if HAVE_DUNE_ALUGRID
                         , Dune::ALUGrid<2, 2, simplex, conforming>
                         , Dune::ALUGrid<2, 2, simplex, nonconforming>
+#endif
+#if HAVE_DUNE_UGGRID
+                        , UGGrid<2>
 #endif
                         >
     GridTypes; // clang-format on
